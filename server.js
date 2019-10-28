f4ad4a4d895c9e0fe3ccaa1e96831ef2b0456031
Emil Félix Muerrle <gitcommit@chaiya.es>
2019-10-28 09:39:38 +0100

    CP1

--- server.js
+++ server.js
@@ -8,30 +8,119 @@ var cors = require('cors');
 
 var app = express();
 
-// Basic Configuration 
+const bodyParser = require('body-parser');
+const dns = require('dns');
+// Basic Configuration
 var port = process.env.PORT || 3000;
 
-/** this project needs a db !! **/ 
+/** this project needs a db !! **/
+
 // mongoose.connect(process.env.MONGOLAB_URI);
 
+// mongoose.connect(
+//   'mongodb+srv://dbUser:<W4ykLQmgjtk8eP2>@cluster0-ehwlj.mongodb.net/test?retryWrites=true&w=majority'
+// );
+
+mongoose
+  .connect('mongodb://localhost/fcc', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  })
+  .then(() => {
+    console.log('Connected to MongoDB...');
+  })
+  .catch(err => {
+    console.error('Could not connect to MongoDB...', err);
+  });
+
+const urlSchema = new mongoose.Schema({
+  url: String,
+  count: Number
+});
+const URL = mongoose.model('URL', urlSchema);
 app.use(cors());
 
 /** this project needs to parse POST bodies **/
 // you should mount the body-parser here
+// app.use(bodyParser.urlencoded());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 app.use('/public', express.static(process.cwd() + '/public'));
 
-app.get('/', function(req, res){
+app.get('/', function(req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
-  
-// your first API endpoint... 
-app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
+const getHostname = url => {
+  const regEx = new RegExp(/^https?\:\/\/([^\/?#]+)(?:[\/?#]|$)/i);
+  const domain = regEx.exec(url);
+
+  if (domain) {
+    return domain[1];
+  }
+  return null;
+};
+
+const util = require('util');
+const dnsLookupAsync = util.promisify(dns.lookup);
+
+const getAddress = async url => {
+  return await dnsLookupAsync(url, '');
+};
+
+const createURL = async body => {
+  // 1. Get the full url and the domain name & check if the submitted url is valid format
+  const hostname = getHostname(body.url);
+  console.log('hostname :', hostname);
+  if (!hostname) {
+    // If hostname is null, return null
+    return null;
+  }
+
+  // 2. Check if the submitted url name points to a valid site (promisified dns.lookup)
+  const { address } = await getAddress(hostname);
+  console.log('address :', await address);
+  if (!address) {
+    // If address is null, return null
+    return null;
+  }
+
+  // 3. Get the latest entry to the db
+  const latestEntry = await URL.findOne().sort({ _id: -1 });
+  // console.log('await latestEntry :', await latestEntry);
+
+  const newEntry = new URL({
+    url: body.url,
+    count: latestEntry._doc.count + 1 || 0
+  });
+
+  try {
+    return await newEntry.save();
+  } catch (err) {
+    console.log(err.message);
+  }
+  return null;
+};
+
+app.post('/api/shorturl/new', async (req, res, next) => {
+  const tempUrl = await createURL(req.body);
+  if (tempUrl) {
+    res.send({ original_url: tempUrl.url, short_url: tempUrl.count });
+    next();
+  } else {
+    res.send({ error: 'invalid URL' });
+  }
 });
 
+app.get('/api/shorturl/:id', async (req, res, next) => {
+  const id = req.params.id;
 
-app.listen(port, function () {
-  console.log('Node.js listening ...');
+  await URL.findOne({ count: id }, (err, doc) => {
+    res.send({ url: doc._doc.url });
+  });
+});
+
+app.listen(port, function() {
+  console.log(`Node.js listening on port ${port}...`);
 });
